d8c20836c025254e6fecca964dea68e88c2f941b
Dany13gd <dany13gd@gmail.com>
2020-04-02 20:58:58 -0600

    Prueba base de datos con MVC

--- Models/MensajeriaAPIModel.Context.cs
+++ Models/MensajeriaAPIModel.Context.cs
@@ -13,10 +13,10 @@ namespace MensajeriaMVC.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Entities : DbContext
+    public partial class MensajeriaEntities : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public MensajeriaEntities()
+            : base("name=MensajeriaEntities")
         {
         }
     
