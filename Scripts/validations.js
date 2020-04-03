455b131515158d6c9354b97983c85a50c2f41364
Eliamar Aguero <eliamar.aguero@janeirodigital.com>
2020-04-02 23:44:05 -0600

    update project views and validations

---
+++ Scripts/validations.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var loc = window.location.pathname;
+    if (loc === '/contactos/Create' || loc.includes('/contactos/Edit')) {
+        $('form').on('submit', (e) => {
+            if ($('#nombre').val() === '' || $('#email').val() === '' || $('#tel_movil').val() === '') {
+                e.preventDefault();
+                alert('Nombre, email y tel móvil son requeridos');
+            }
+        });
+    }
+})();
